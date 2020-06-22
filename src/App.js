5d3d093a1b5f8bf3e4bf76d87fae2aaec622504a
Naveen <rajnaveen677@gmail.com>
2020-06-22 12:09:24 +0530

    #1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import QuizData from "./Components/Data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answer: [],
+      currentIndex: 0,
+      options: [],
+      score: 5,
+    };
+  }
+  componentDidMount() {
+    this.loadQuestion();
+  }
+
+  loadQuestion() {
+    const { currentIndex } = this.state;
+    this.setState(() => {
+      return {
+        question: QuizData[currentIndex].question,
+        options: QuizData[currentIndex].options,
+        answer: QuizData[currentIndex].answer,
+      };
+    });
+  }
+  goToNextQuestion() {
+    if (this.state.currentIndex === this.state.answer) {
+      this.setState({
+        score: this.state.score + 1,
+      });
+      this.setState({
+        currentIndex: this.state.currentIndex + 1,
+      });
+    }
+  }
+  checkAnswer(e) {
+    if (e.value !== this.state.answer) {
+      this.setState({
+        answer: this.state.answer,
+      });
+    }
+  }
+
+  render() {
+    const { question, options, answer, score } = this.state;
+    {
+      return (
+        <div className="container">
+          <h3>
+            Your Score is <span className="badge badge-info">{score}</span>
+            Points
+          </h3>
+          <h2>{question}</h2>
+          <p>{answer}</p>
+          <ul>
+            {options.map((option) => (
+              <p value="answer" onClick={this.checkAnswer}>
+                <li>{option}</li>
+              </p>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
 }
-
 export default App;
